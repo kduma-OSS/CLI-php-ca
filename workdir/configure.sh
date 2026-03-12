117ec15d9c3e003be5f05e1c50b859d5c65e040d
Krystian Duma <kduma@users.noreply.github.com>
2026-03-12 22:30:49 +0100

    feat(scripts): add `configure.sh `, for example, working directory

---
+++ workdir/configure.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+ensure_key() {
+    local ca_file="$1"
+
+    printf "[%s] Checking if 'ca' private key exists: " "$ca_file"
+    if ! ../php-ca key:has ca --ca="$ca_file" --quiet; then
+        echo "no"
+        printf "[%s] Generating new private key: " "$ca_file"
+        if ! ../php-ca key:make ca --ca="$ca_file"; then
+            echo "failed"
+            return 1
+        else
+            echo "generated"
+        fi
+    else
+        echo "yes"
+    fi
+}
+
+ensure_key root-ca.json || exit 1
+ensure_key sub-ca.json || exit 1
+ensure_key int-ca.json || exit 1
