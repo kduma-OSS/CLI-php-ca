c8e19a0c63d2986a23033856a72075b68806b448
Krystian Duma <kduma@users.noreply.github.com>
2026-03-13 00:45:07 +0100

    feat(configure): add support for configurable key sizes in `ensure_key`
    Allow specifying key size when creating CA private keys in `configure.sh`, with updates to support `root-ca`, `sub-ca`, and `int-ca` configurations.

--- workdir/configure.sh
+++ workdir/configure.sh
@@ -2,12 +2,13 @@
 
 ensure_key() {
     local ca_file="$1"
+    local key_size="$2"
 
     printf "[%s] Checking if 'ca' private key exists: " "$ca_file"
     if ! ../php-ca key:exists ca --ca="$ca_file" --quiet; then
         echo "no"
         printf "[%s] Generating new private key: " "$ca_file"
-        if ! ../php-ca key:create ca --ca="$ca_file"; then
+        if ! ../php-ca key:create ca --ca="$ca_file" --key-size="$key_size"; then
             echo "failed"
             return 1
         else
@@ -38,11 +39,11 @@ ensure_self_signed_ca() {
     fi
 }
 
-ensure_key root-ca.json || exit 1
+ensure_key root-ca.json 4096 || exit 1
 ensure_self_signed_ca root-ca.json "C=WW, O=PHP PKI CA Project, CN=My Root CA" "+25 years" || exit 1
 
-ensure_key sub-ca.json || exit 1
+ensure_key sub-ca.json 2048 || exit 1
 
 
 
-ensure_key int-ca.json || exit 1
+ensure_key int-ca.json 1024 || exit 1
