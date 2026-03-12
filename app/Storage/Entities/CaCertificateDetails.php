dca14281a40050436dd788bf3724d6bd56e080d6
Krystian Duma <kduma@users.noreply.github.com>
2026-03-13 00:12:03 +0100

    feat(storage): extend `CaCertificateDetails` with validity period fields
    Add `valid_from` and `valid_to` fields to `CaCertificateDetails` entity to represent certificate validity period. Update array serialization and deserialization methods to handle new fields.

--- app/Storage/Entities/CaCertificateDetails.php
+++ app/Storage/Entities/CaCertificateDetails.php
@@ -2,12 +2,16 @@
 
 namespace App\Storage\Entities;
 
+use Carbon\CarbonImmutable;
+
 class CaCertificateDetails
 {
     public function __construct(
         public readonly string $serial_number,
         public readonly string $distinguished_name,
         public readonly ?int $path_length_constraint = null,
+        public readonly CarbonImmutable $valid_from,
+        public readonly CarbonImmutable $valid_to,
     ) {}
 
     public static function fromArray(array $data): static
@@ -16,6 +20,8 @@ class CaCertificateDetails
             serial_number: $data['serial_number'],
             distinguished_name: $data['distinguished_name'],
             path_length_constraint: $data['path_length_constraint'] ?? null,
+            valid_from: new CarbonImmutable($data['valid_from']),
+            valid_to: new CarbonImmutable($data['valid_to']),
         );
     }
 
@@ -25,6 +31,8 @@ class CaCertificateDetails
             'serial_number' => $this->serial_number,
             'distinguished_name' => $this->distinguished_name,
             'path_length_constraint' => $this->path_length_constraint,
+            'valid_from' => $this->valid_from->format('c'),
+            'valid_to' => $this->valid_to->format('c'),
         ];
     }
 }
