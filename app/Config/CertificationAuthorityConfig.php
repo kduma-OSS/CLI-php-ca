40022281a3ac9e1a72f9b173b0198d902e62a023
Krystian Duma <kduma@users.noreply.github.com>
2026-03-13 01:46:32 +0100

    refactor(config): remove `distinguished_name` from CA configuration and update related files
    Remove unused `distinguished_name` field from CA configuration files and `CertificationAuthorityConfig` class.

--- app/Config/CertificationAuthorityConfig.php
+++ app/Config/CertificationAuthorityConfig.php
@@ -6,7 +6,6 @@ readonly class CertificationAuthorityConfig
 {
     public function __construct(
         public bool $randomSerialNumbers,
-        public string $distinguishedName,
         public array $certificateDistributionPoints,
         public array $crlDistributionPoints,
     ) {}
@@ -15,7 +14,6 @@ readonly class CertificationAuthorityConfig
     {
         return new self(
             randomSerialNumbers: $data['random_serial_numbers'],
-            distinguishedName: $data['distinguished_name'],
             certificateDistributionPoints: $data['certificate_distribution_points'] ?? [],
             crlDistributionPoints: $data['crl_distribution_points'] ?? [],
         );
