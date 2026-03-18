b21c7b97a52aa466612fcdb5a7cfaabfc57cdfda
Krystian Duma <kduma@users.noreply.github.com>
2026-03-19 00:05:34 +0100

    Configure RSA OpenSSL path for Phar builds

--- src/cli/app/Providers/AppServiceProvider.php
+++ src/cli/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use KDuma\PhpCA\ConfigManager\ConfigManagerRegistry;
+use phpseclib3\Crypt\RSA;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ConfigManagerRegistry::registerDefaults();
+
+        if (\Phar::running() !== '') {
+            $pharOpenSslConf = \Phar::running() . '/vendor/phpseclib/phpseclib/phpseclib/openssl.cnf';
+            $tmpConf = sys_get_temp_dir() . '/phpseclib_openssl.cnf';
+
+            if (! file_exists($tmpConf) || md5_file($tmpConf) !== md5_file($pharOpenSslConf)) {
+                copy($pharOpenSslConf, $tmpConf);
+            }
+
+            RSA::setOpenSSLConfigPath($tmpConf);
+        }
     }
 
     /**
